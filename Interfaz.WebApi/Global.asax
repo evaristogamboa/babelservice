b929e132d141d839b1dbb9b1d5b35ceceac29b15
Guillermo Carrillo <gcarrillo@nubise.com>
2015-05-04 18:47:36 -0430

    Se agregan carpetas del proyecto WebApi

---
+++ Interfaz.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nubise.Hc.Utils.I18n.Babel.Interfaz.WebApi.WebApiApplication" Language="C#" %>
---
+++ Interfaz.WebApi/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using Nubise.Hc.Utils.I18n.Babel.Interfaz.WebApi.AppStart;
+
+namespace Nubise.Hc.Utils.I18n.Babel.Interfaz.WebApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}

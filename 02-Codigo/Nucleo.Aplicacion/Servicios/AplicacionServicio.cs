77ab2b1f440986af73a3f43e791aee801d9f3fac
Yomaira Cabezas <ycabezas@nubise.com>
2015-05-13 16:24:55 -0430

    Se agregaron archivos para pruebas unitarias

---
+++ 02-Codigo/Nucleo.Aplicacion/Servicios/AplicacionServicio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Babel.Nucleo.Dominio.Entidades.Diccionario;
+
+namespace Babel.Nucleo.Aplicacion.Servicios
+{
+    class AplicacionServicio
+    {
+
+    }
+}

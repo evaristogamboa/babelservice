fd303c92d952cb530000cb391e3b0507ed940c0f
Michelle Gonzalez <migonzalez@nubise.com>
2015-05-14 11:49:31 -0430

    se hacen merge de los cambios de michelle

---
+++ 02-Codigo/Nucleo.Aplicacion/Modelos/Response/ModificarTraduccionesAUnaEtiquetaDeUnDiccionarioRespuesta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Babel.Nucleo.Aplicacion.Fachada
+{
+	public class ModificarTraduccionesAUnaEtiquetaDeUnDiccionarioRespuesta
+	{
+	}
+}

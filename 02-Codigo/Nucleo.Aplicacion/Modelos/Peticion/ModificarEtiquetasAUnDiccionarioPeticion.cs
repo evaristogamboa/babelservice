6835d16eaf4a09f6ec3224bc71082f0859d5cb7e
Yomaira Cabezas <ycabezas@nubise.com>
2015-05-14 17:24:45 -0430

    Se agregaron propiedas y constructores a las clases modificadas.

--- 02-Codigo/Nucleo.Aplicacion/Modelos/Peticion/ModificarEtiquetasAUnDiccionarioPeticion.cs
+++ 02-Codigo/Nucleo.Aplicacion/Modelos/Peticion/ModificarEtiquetasAUnDiccionarioPeticion.cs
@@ -2,10 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Babel.Nucleo.Dominio.Entidades.Etiquetas;
 
-namespace Babel.Nucleo.Aplicacion.Fachada
+namespace Babel.Nucleo.Aplicacion.Modelos.Peticion
 {
 	public class ModificarEtiquetasAUnDiccionarioPeticion
-	{
-	}
+    {
+
+        #region Propiedades
+
+        /// <summary>
+        /// Obtiene o establece el identificador del Diccionario al cual se le moficarán las Etiquetas
+        /// </summary>
+        public Guid DiccionarioId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Etiqueta> ListaDeEtiquetaDeDiccionario { get; set; }
+
+        #endregion
+
+        #region Constructores
+
+	    /// <summary>
+	    /// 
+	    /// </summary>
+        private ModificarEtiquetasAUnDiccionarioPeticion()
+	    {
+	        
+	    }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+	    public static ModificarEtiquetasAUnDiccionarioPeticion CrearNuevaInstancia()
+	    {
+	        return new ModificarEtiquetasAUnDiccionarioPeticion();
+	    }
+
+        #endregion
+
+    }
 }

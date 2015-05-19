694b2afe89a4e3162b876934fb1c1af323d49467
Andres Secondulfo <asecondulfo@nubise.com>
2015-05-18 19:33:46 -0430

    Agregaron archivos perdidos

---
+++ 02-Codigo/Interfaz.WebApi/AppStart/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+
+namespace Babel.Interfaz.WebApi.AppStart
+{
+	public static class WebApiConfig
+	{
+		public static void Register (HttpConfiguration config)
+		{
+			// Configuración y servicios de API web
+
+			// Rutas de API web
+			config.MapHttpAttributeRoutes ();			
+		}
+	}
+}

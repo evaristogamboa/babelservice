53a13de6ef286cd86e200c5f9714c51e56141d4d
Michelle Gonzalez <migonzalez@nubise.com>
2015-05-21 11:50:03 -0430

    Se realizaron ajustes a las pruebas existentes y se crearon nuevos métodos en la fachada para realizar consultas de etiquetas por los atributos descripción, estatus e idioma por defecto.

---
+++ 02-Codigo/Nucleo.Aplicacion/Modelos/Respuesta/ConsultarEtiquetasDeDiccionarioPorIdiomaPorDefectoRespuesta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Babel.Nucleo.Dominio.Entidades.Etiquetas;
+
+namespace Babel.Nucleo.Aplicacion.Modelos.Respuesta
+{
+	public class ConsultarEtiquetasDeDiccionarioPorIdiomaPorDefectoRespuesta
+	{
+		public List<Etiqueta> ListaDeEtiquetas { get; set; }
+
+		public Dictionary<string, Guid> Relaciones { get; set; }
+
+		public ModeloRespuesta Respuesta { get; set; }
+
+		#region constructores
+
+		private ConsultarEtiquetasDeDiccionarioPorIdiomaPorDefectoRespuesta()
+		{
+			Relaciones = new Dictionary<string, Guid> { { "diccionario", Guid.Empty } };
+			ListaDeEtiquetas = new List<Etiqueta>();
+			Respuesta = new ModeloRespuesta();
+		}
+
+		public static ConsultarEtiquetasDeDiccionarioPorIdiomaPorDefectoRespuesta CrearNuevaInstancia()
+		{
+			return new ConsultarEtiquetasDeDiccionarioPorIdiomaPorDefectoRespuesta();
+		}
+
+		#endregion
+	}
+}

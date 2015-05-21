f7becdf2bb793d031dfa4d9a56a365dd67fe1c9a
Andres Secondulfo <asecondulfo@nubise.com>
2015-05-21 17:24:14 -0430

    no message

--- 02-Codigo/Interfaz.WebApi/Modelos/Peticion/EliminarUnDiccionarioPeticion.cs
+++ 02-Codigo/Interfaz.WebApi/Modelos/Peticion/EliminarUnDiccionarioPeticion.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using app = Babel.Nucleo.Aplicacion.Modelos.Peticion;
+using comunes = Babel.Interfaz.WebApi.Modelos.Comunes;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Babel.Interfaz.WebApi.Modelos.Peticion
 {
 	public class EliminarUnDiccionarioPeticion
 	{
-	}
+
+        public comunes.Diccionarios Diccionario { get; set; }
+        public app.EliminarUnDiccionarioPeticion AppDiccionarioPeticion { get; set; }
+
+        private EliminarUnDiccionarioPeticion(HttpRequestMessage peticionHttp)
+        {
+            //Diccionario = JsonConvert.des
+            this.AppDiccionarioPeticion = app.EliminarUnDiccionarioPeticion.CrearNuevaInstancia();
+            //this.AppDiccionarioPeticion.DiccionarioId = 
+        }
+
+        public static EliminarUnDiccionarioPeticion CrearUnaNuevaPeticionDeEliminar(HttpRequestMessage peticionHttp)
+        {
+            return new EliminarUnDiccionarioPeticion(peticionHttp);
+        }
+    }
 }
